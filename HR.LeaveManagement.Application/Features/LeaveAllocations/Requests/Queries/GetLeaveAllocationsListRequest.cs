60d6ae376655e1fcc51bb467d224553464fd683d
Anold Kupara <kuparaanold20@gmail.com>
2022-10-16 08:47:55 +0200

    added the GetLeaveAllocationsListRequest and its relative handler

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Queries/GetLeaveAllocationsListRequest.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveAllocations;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Queries
+{
+    public class GetLeaveAllocationsListRequest : IRequest<List<LeaveAllocationDto>>
+    {
+    }
+}
