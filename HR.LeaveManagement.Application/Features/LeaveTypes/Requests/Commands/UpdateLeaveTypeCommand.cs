bc6c99551999438f05de66ad66547272ab58a262
Anold Kupara <kuparaanold20@gmail.com>
2022-10-17 13:01:39 +0200

    added new specific DTO's

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Requests/Commands/UpdateLeaveTypeCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands
+{
+    public class UpdateLeaveTypeCommand  
+    {
+    }
+}
