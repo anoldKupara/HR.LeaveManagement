03141e48b31fd6320db1fcbe141057f843526a3e
Anold Kupara <kuparaanold20@gmail.com>
2022-10-21 15:22:54 +0200

    added the PersistenceServicesRegistration class

---
+++ HR.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,28 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<HrLeaveManagementDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+
+            return services;
+        }
+    }
+}
