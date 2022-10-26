fff59cec10ecfe721c3373058434a0bf4fe3675e
Anold Kupara <kuparaanold20@gmail.com>
2022-10-26 03:04:18 +0200

    updated the mapping profiles

--- HR.LeaveManagement.Application/Profiles/MappingProfiles.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfiles.cs
@@ -13,10 +13,24 @@ namespace HR.LeaveManagement.Application.Profiles
     {
         public MappingProfiles()
         {
+            #region LeaveAllocation
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
+
+            #region LeaveRequest
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
-            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, ChangeLeaveRequestApprovalDto>().ReverseMap();
+            #endregion
+
+            #region LeaveType
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            #endregion
         }
     }
 }
