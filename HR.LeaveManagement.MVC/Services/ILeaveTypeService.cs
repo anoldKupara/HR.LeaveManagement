db311290ecb55dc86f003b409fb57a1e103d0539
Anold Kupara <kuparaanold20@gmail.com>
2022-11-02 11:50:06 +0200

    added ILeaveTypeService

---
+++ HR.LeaveManagement.MVC/Services/ILeaveTypeService.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.MVC.Models;
+using HR.LeaveManagement.MVC.Services.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.MVC.Services
+{
+    public interface ILeaveTypeService
+    {
+        Task<List<LeaveTypeVM>> GetLeaveTypes();
+        Task<LeaveTypeVM> GetLeaveTypeDetails();
+        Task<Response<int>>CreateLeaveType(CreateLeaveTypeVM leaveType);
+        Task UpdateLeaveType(LeaveTypeVM leaveType);
+        Task DeleteLeaveType(LeaveTypeVM leaveType);
+    }
+}
